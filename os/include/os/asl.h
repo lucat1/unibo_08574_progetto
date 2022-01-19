67080a930f8e5e1473b2ed95318ad716292a6abb
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-01-19 02:28:06 +0100

    Add sample doc comments

--- os/include/os/asl.h
+++ os/include/os/asl.h
@@ -1,5 +1,15 @@
+/** \file
+ * \brief Active Semaphore List
+ *
+ * \author Luca Tagliavini
+ * \author Stefano Volpe
+ * \date 17-01-2022
+ */
+
 #ifndef PANDOS_ASL_H
 #define PANDOS_ASL_H
 
+/// Placeholder. \todo Remove me.
 int test2();
+
 #endif /* PANDOS_ASL_H */
--- os/include/os/pcb.h
+++ os/include/os/pcb.h
@@ -1,6 +1,15 @@
+/** \file
+ * \brief Process Control Block
+ *
+ * \author Alessandro Frau
+ * \author Gianmaria Rovelli
+ * \date 17-01-2022
+ */
+
 #ifndef PANDOS_PCB_H
 #define PANDOS_PCB_H
 
+/// Placeholder. \todo Remove me.
 int test1();
 
 #endif /* PANDOS_PCB_H */
