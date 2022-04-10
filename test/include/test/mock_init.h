bfd8be1bb3b94f5358dabac0d2ea371db65d7e3b
Luca <luca.tagliavini5@studio.unibo.it>
2022-04-10 19:06:01 +0200

    mock init process and add first syscall test

---
+++ test/include/test/mock_init.h
@@ -0,0 +1,31 @@
+/**
+ * \file mock_init.h
+ * \brief Mock the nucleus init sequence to instantiate common structures needed
+ * for tests.
+ *
+ * \author Luca Tagliavini
+ * \date 10-04-2022
+ */
+
+#ifndef PANDOS_TEST_MOCK_INIT_H
+#define PANDOS_TEST_MOCK_INIT_H
+
+#include "os/asl.h"
+#include "os/pcb.h"
+#include "os/scheduler.h"
+#include "os/scheduler_impl.h"
+#include "os/semaphores.h"
+#include "test/mock_devices.h"
+#include "test/mock_processor.h"
+
+void mock_init()
+{
+    init_scheduler();
+    init_semaphores();
+    init_pcbs();
+    init_asl();
+    reset_devices();
+    reset_processor();
+}
+
+#endif /* PANDOS_TEST_MOCK_INIT_H */
--- test/src/syscall_get_process_id.c
+++ test/src/syscall_get_process_id.c
@@ -1,23 +1,26 @@
 #include "os/scheduler.h"
-#include "test/mock_devices.h"
+#include "test/mock_init.h"
 #include "test/mock_iodev.h"
-#include "test/mock_processor.h"
 #include "test/mock_syscall.h"
 #include "test/test.h"
 
 int main()
 {
+    mock_init();
     it("sanitizes the input parameter")
     {
         active_process = spawn_process(false);
+        assert(active_process->p_pid != -1);
         SYSCALL(GETPROCESSID, (size_t)NULL, 0, 0);
         assert(active_process->p_pid == -1);
 
         active_process = spawn_process(false);
+        assert(active_process->p_pid != -1);
         SYSCALL(GETPROCESSID, -1, 0, 0);
         assert(active_process->p_pid == -1);
 
         active_process = spawn_process(false);
+        assert(active_process->p_pid != -1);
         SYSCALL(GETPROCESSID, 2, 0, 0);
         assert(active_process->p_pid == -1);
     }
