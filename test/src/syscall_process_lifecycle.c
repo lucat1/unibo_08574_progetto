0e90021d7971b80224f241c8f2fffd25ebc12a26
Stefano Volpe <volpestefano@outlook.com>
2022-04-12 17:27:46 +0200

    use NULL_PID in syscall_process_lifecycle test

--- test/src/syscall_process_lifecycle.c
+++ test/src/syscall_process_lifecycle.c
@@ -1,17 +1,18 @@
+#include "os/const.h"
+#include "os/pcb.h"
 #include "os/scheduler.h"
 #include "os/syscall.h"
 #include "os/types.h"
-#include "test/mock_init.h"
-#include "test/mock_syscall.h"
-#include "test/mock_iodev.h"
-#include "test/test.h"
-#include "os/const.h"
 #include "os/util.h"
+#include "test/mock_init.h"
+#include "test/mock_iodev.h"
+#include "test/mock_syscall.h"
+#include "test/test.h"
 #include <stdio.h>
 /* TESTING NSYS1 & NSYS2 */
 
-void p1(){
-    /* It is not supposed to do anything*/
+void p1()
+{ /* It is not supposed to do anything*/
 }
 
 int main()
@@ -19,14 +20,14 @@ int main()
     mock_init();
     active_process = spawn_process(false);
     state_t proc1;
-    set_state(&proc1, (memaddr) p1);
+    set_state(&proc1, (memaddr)p1);
     it("correctly creates a new high priority process")
     {
-        assert(active_process->p_pid != -1);
+        assert(active_process->p_pid != NULL_PID);
 
         SYSCALL(CREATEPROCESS, (size_t)&proc1, true, 0);
 
-        assert(active_process->p_s.reg_v0 != -1);
+        assert(active_process->p_s.reg_v0 != NULL_PID);
         assert(running_count == 2);
         assert(!list_empty(&active_process->p_child));
         assert(!list_empty(&ready_queue_hi));
@@ -42,11 +43,11 @@ int main()
     }
     it("correctly creates a new low priority process")
     {
-        assert(active_process->p_pid != -1);
+        assert(active_process->p_pid != NULL_PID);
 
         SYSCALL(CREATEPROCESS, (size_t)&proc1, false, 0);
 
-        assert(active_process->p_s.reg_v0 != -1);
+        assert(active_process->p_s.reg_v0 != NULL_PID);
         assert(running_count == 2);
         assert(!list_empty(&active_process->p_child));
         assert(!list_empty(&ready_queue_lo));
@@ -60,17 +61,17 @@ int main()
         assert(list_empty(&active_process->p_child));
         assert(list_size(&ready_queue_lo) == 1);
     }
-    it("correctly gets out of memory and returns -1")
+    it("correctly gets out of memory and returns NULL_PID")
     {
         /* There is already 1 process allocated from previous code, so we need
          * To add at least 19 more processes to get out of memory
          */
-        for(int i = 0; i<19; i++){
+        for (int i = 0; i < 19; i++) {
             SYSCALL(CREATEPROCESS, (size_t)&proc1, true, 0);
         }
-        assert(active_process->p_s.reg_v0 != -1);
+        assert(active_process->p_s.reg_v0 != NULL_PID);
         SYSCALL(CREATEPROCESS, (size_t)&proc1, true, 0);
-        assert(active_process->p_s.reg_v0 == -1);
+        assert(active_process->p_s.reg_v0 == NULL_PID);
     }
     return 0;
 }
