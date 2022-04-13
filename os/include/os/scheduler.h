83ca305bbdc090517650383117cd0e4e1dac6b8d
Stefano Volpe <volpestefano@outlook.com>
2022-04-14 00:49:38 +0200

    Add scheduler doc comments

--- os/include/os/scheduler.h
+++ os/include/os/scheduler.h
@@ -33,14 +33,58 @@ extern size_t get_recycle_count();
  * \return The allocated process descriptor.
  */
 extern pcb_t *spawn_process(bool priority);
+
+/**
+ * \brief Recursively kills a process and all of its progeny. This procedure
+ * abruptly comes to a halt on the first failed kill.
+ * \param[in,out] p Pointer to the PCB whose progeny is to be killed.
+ * \return 0 on success, 1 on NULL input, 2 if input could not be removed from
+ * its parent, or 3 if any of its descendants could not be killed.
+ */
 extern int kill_progeny(pcb_t *p);
 
+/**
+ * \brief Enqueues a process on the appropriate queue based on its priority.
+ * \param[in,out] p Pointer to the PCB of the process to be enqueued: if NULL,
+ * nothing happens.
+ */
 extern void enqueue_process(pcb_t *p);
+
+/**
+ * \brief Removes a given process from the queue it is currently in.
+ * \param[in] p Pointer to the PCB of the process to be removed.
+ * \return A pointer to the PCB of the removed process, or NULL on failure.
+ */
 extern pcb_t *dequeue_process(pcb_t *p);
+
+/**
+ * \brief Retrieves a process from its pid.
+ * \param[in] pid The pid whose process is to be retrieved.
+ * \return A pointer to the PCB of the desired process, or NULL if such a
+ * process does not currently exist.
+ */
 extern pcb_t *const find_process(pandos_pid_t pid);
 
+/**
+ * \brief Initializes the state variables and the data structures used by the
+ * scheduler.
+ */
 extern void init_scheduler();
+
+/**
+ * \brief Decides which process should be running next.
+ * \param[in,out] pcb An optional pointer to a PCB describing a new process the
+ * scheduler should now take in account.
+ * \param[in] enqueue When the first parameter is specified, tells whether it
+ * should be enqueued or immediately set as running process.
+ */
 void schedule(pcb_t *pcb, bool enqueue);
+
+/**
+ * \brief Prints a formatted message before making the kernel panic.
+ * \param[in] fmt The format string to be printed as message.
+ * \param[in] ... Additional parameters for the format string.
+ */
 void scheduler_panic(const char *fmt, ...);
 
 #endif /* PANDOS_OS_SCHEDULER_H */
