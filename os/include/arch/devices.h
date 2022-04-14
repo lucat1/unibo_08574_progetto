0c8a4d6326de8ffd47f361658feee2f915f95ae6
Stefano Volpe <volpestefano@outlook.com>
2022-04-14 12:33:25 +0200

    devices doc comments

--- os/include/arch/devices.h
+++ os/include/arch/devices.h
@@ -18,19 +18,38 @@
 #include <umps/const.h>
 #endif
 
+/** A descriptor for a I/O device. */
 typedef struct iodev {
+    /** The semaphore associated to this device. */
     int *semaphore;
+    /** The interrupt line associated to this device. */
     int interrupt_line;
 } iodev_t;
 
-extern iodev_t _get_iodev(size_t *cmd_addr);
+/**
+ * \brief Retrieves the appropriate descriptor for the desired I/O device.
+ * \param[in] cmd_addr The address of the command from which to infer the
+ * device.
+ * \return The desired I/O device descriptor.
+ */
 extern iodev_t get_iodev(size_t *cmd_addr);
 extern void status_il_on_all(size_t *prev);
 extern void status_il_on(size_t *prev, int line);
 
-/* tod = time of day */
+/** \brief Stores the time of day.
+ * \param[out] time The address of the variable where the time of day is to be
+ * stored.
+ */
 extern void store_tod(int *time);
+
+/** \brief Loads a new value for the interval timer.
+ * \param[in] time The new value for the interval timer.
+ */
 extern void load_interval_timer(int time);
+
+/** \brief Loads a new value for the local timer.
+ * \param[in] time The new value for the local timer.
+ */
 extern void load_local_timer(int time);
 
 #endif /* PANDOS_ARCH_DEVICES_H */
