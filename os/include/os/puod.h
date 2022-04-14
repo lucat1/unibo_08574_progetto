1e1fce55e424850cc4b4fad4a0a32089919583ee
Stefano Volpe <volpestefano@outlook.com>
2022-04-14 11:44:28 +0200

    puod.h doc comments

--- os/include/os/puod.h
+++ os/include/os/puod.h
@@ -12,6 +12,14 @@
 
 #include "os/scheduler.h"
 
+/**
+ * \brief Passes the control up to the appropriate handler specified by the
+ * active process, or kills the active process and all of its progeny if such a
+ * handler does not exist.
+ * \param[in] type Specifies which old state exception and which new context
+ * should be used.
+ * \return Always CONTROL_BLOCK.
+ */
 extern scheduler_control_t pass_up_or_die(memaddr type);
 
 #endif /* PANDOS_OS_PUOD_H */
