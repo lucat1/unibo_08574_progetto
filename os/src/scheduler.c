e5490627416113f433517047a0c3e336cbf0e4b3
Alessandro Frau <frau.alessandro01@gmail.com>
2022-04-18 12:56:48 +0200

    Removed some prints

--- os/src/scheduler.c
+++ os/src/scheduler.c
@@ -124,7 +124,6 @@ inline void init_scheduler()
 
 static inline void scheduler_wait()
 {
-    pandos_kprintf("-- WAIT\n");
     active_process = NULL;
     reset_timer();
 
@@ -140,7 +139,6 @@ static inline void scheduler_wait()
 
 static inline void scheduler_takeover()
 {
-    pandos_kprintf(">> TAKEOVER(%d)\n", active_process->p_pid);
     status_interrupts_on_process(&active_process->p_s.status);
     reset_local_timer();
     /* Disable the processor Local Timer on hi processes */
@@ -162,7 +160,6 @@ static inline void wait_or_die()
 
 void schedule(pcb_t *pcb, bool enqueue)
 {
-    pandos_kprintf("-- SCHEDULE(%p, %s)\n", pcb, enqueue ? "true" : "false");
     if (enqueue && pcb != NULL) {
         enqueue_process(pcb);
     }
