8fccdaf8f132a705173148c93215c1567e67aa88
Stefano Volpe <volpestefano@outlook.com>
2022-07-10 18:27:38 +0200

    fix fsysprintf: termreg_t * -> index when calling __pandos_printf

--- phase3/src/print.c
+++ phase3/src/print.c
@@ -1,7 +1,7 @@
 #include "support/print.h"
+#include "os/semaphores.h"
 #include "os/util.h"
 #include "os/util_impl.h"
-#include "os/semaphores.h"
 #include <umps/arch.h>
 
 /* hardware constants */
@@ -15,10 +15,10 @@ size_t syscall_writer(void *termid, const char *msg, size_t len)
 {
     typedef unsigned int devregtr;
     const char *s = msg;
-    devregtr *base = (devregtr *)(DEV_REG_ADDR(IL_TERMINAL, (int)termid));
-    devregtr *command = base + 3;
+    memaddr *command = DEVICE_COMMAND(IL_TERMINAL, (int)termid);
     devregtr status;
 
+    sem_term_mut = get_semaphore(IL_TERMINAL, (int)termid, true);
     SYSCALL(PASSEREN, (int)&sem_term_mut, 0, 0); /* P(sem_term_mut) */
     while (*s != EOS) {
         devregtr value = PRINTCHR | (((devregtr)*s) << 8);
@@ -34,11 +34,10 @@ size_t syscall_writer(void *termid, const char *msg, size_t len)
 
 size_t fsysprintf(int termid, const char *fmt, ...)
 {
-    termreg_t *term = (termreg_t *)DEV_REG_ADDR(IL_TERMINAL, termid);
     sem_term_mut = get_semaphore(IL_TERMINAL, termid, false);
     va_list varg;
     va_start(varg, fmt);
-    size_t res = __pandos_printf(term, syscall_writer, fmt, varg);
+    size_t res = __pandos_printf((void *)termid, syscall_writer, fmt, varg);
     va_end(varg);
     return res;
 }
