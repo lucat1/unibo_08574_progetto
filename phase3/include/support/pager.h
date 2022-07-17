24e74570f181eaa971ee22507a614b373f2bb260
Stefano Volpe <volpestefano@outlook.com>
2022-07-17 14:32:42 +0200

    Doc comments in phase3 headers

--- phase3/include/support/pager.h
+++ phase3/include/support/pager.h
@@ -1,3 +1,13 @@
+/**
+ * \file pager.h
+ * \brief The pager module
+ *
+ * \author Gianmaria Rovelli
+ * \author Luca Tagliavini
+ * \author Stefano Volpe
+ * \date 26-06-2022
+ */
+
 #ifndef PANDOS_PAGER_H
 #define PANDOS_PAGER_H
 
@@ -5,17 +15,47 @@
 #include "os/const.h"
 #include "os/types.h"
 
+/** Type describing a Page Table as an array of Page Table Entries. */
 typedef pte_entry_t pte_entry_table[MAXPAGES];
 
+/**
+ * \brief Allocate the Swap Pool and initialize the Swap Pool Table.
+ */
 extern void init_pager();
 
+/**
+ * \brief Initialize a Page Table for a new U-Proc.
+ * \param[out] page_table The Page Table to be initialized.
+ * \param[in] asid The ASID of the U-Proc.
+ * \return True on success, false otherwise.
+ */
 extern bool init_page_table(pte_entry_table page_table, int asid);
+
+/**
+ * \brief Mark a U-Proc's frames in the Swap Pool Table as initialized, and
+ * make him release the Swap Pool semaphore if necessary.
+ * \param[in] asid The ASID of the U-Proc.
+ */
 extern void clean_after_uproc(int asid);
 
+/**
+ * \brief A handler for TLB-Refill events.
+ */
 extern void tlb_refill_handler();
+
+/**
+ * \brief A handler for TLB Management exceptions.
+ */
 extern void support_tlb();
 
-extern void active_interrupts();
+/**
+ * \brief Disable the interrups, starting a new critical session.
+ */
 extern void deactive_interrupts();
+
+/**
+ * \brief Enable interrupts back, ending the current critical session.
+ */
+extern void active_interrupts();
 
 #endif /* PANDOS_PAGER_H */
--- phase3/include/support/support.h
+++ phase3/include/support/support.h
@@ -1,11 +1,38 @@
+/**
+ * \file support.h
+ * \brief Non-TLB exceptions handling and Support Level semaphores
+ *
+ * \author Gianmaria Rovelli
+ * \author Luca Tagliavini
+ * \author Stefano Volpe
+ * \date 05-05-2022
+ */
+
 #ifndef PANDOS_SUPPORT_H
 #define PANDOS_SUPPORT_H
 
 #include "arch/processor.h"
 
+/**
+ * \briefs Initialize all mutex for uninterrupted streaming devices sequential
+ * access.
+ */
 extern void init_sys_semaphores();
+
+/**
+ * \brief Perform a PASSEREN operation of the Master Semaphore: that is,
+ * wait for a U-Proc to terminate.
+ */
 extern void master_semaphore_p();
+
+/**
+ * \brief A general exception handler for the Support Level.
+ */
 extern void support_generic();
+
+/**
+ * \brief A Support Level exception handler for Program Trap exceptions.
+ */
 extern void support_trap();
 
 #endif /* PANDOS_SUPPORT_H */
